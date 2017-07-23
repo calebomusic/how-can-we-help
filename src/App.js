a42ad59634e5f27a7b0105568a99c8c046ffe585
calebomusic <calebomusic@gmail.com>
2017-07-23 12:22:19 -0700

    Add transitions, renderAllCards, renderBackButton, fix bugs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,88 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Cards from './cards';
+import QuestionCards from './questionCards';
+import AnswerCards from './answerCards'
 import CARDS from './cardData';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      cards: CARDS["question"][0]
+      cards: [ CARDS["question"][0] ]
     }
 
-    this.onClick = this.onClick.bind(this);
+    this.onClickCards = this.onClickCards.bind(this);
+    this.onClickBackButton = this.onClickBackButton.bind(this);
+    this.renderAllCards = this.renderAllCards.bind(this);
+    this.onClickBackButton = this.onClickBackButton.bind(this);
   }
 
-  onClick(type, id) {
-    this.setState({cards: CARDS[type][id]});
+  componentDidUpdate() {
+    document.getElementById(`cards-${this.state.cards.length-1}`).scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+
+  onClickCards(type, id, optionId) {
+    const lastCards = this.state.cards[this.state.cards.length-1];
+    
+    if (lastCards.options && lastCards.options[optionId].nextId === id) {
+      this.setState({
+        cards: this.state.cards.concat(CARDS[type][id])
+      });
+    }
+  }
+
+  renderBackButton() {
+    if (this.state.cards.length > 1) {
+      return (
+        <div className="fixed">
+          <div className="pos-back-button"></div>
+          <a className="btn right indigo darken-4 text-white" onClick={this.onClickBackButton}>
+            <i className="material-icons right">undo</i>Back</a>
+        </div>
+      );
+    } else {
+      return <a></a>
+    }
+  }
+
+  onClickBackButton() {
+    if (this.state.cards.length > 1) {
+      this.setState({
+        cards: this.state.cards.slice(0, this.state.cards.length-1)
+      })
+    }
+  }
+
+  renderAllCards() {
+    const allCards = [];
+
+    this.state.cards.forEach((cards, i) => {
+      if (cards.body) {
+        allCards.push(
+              <li className="card" key={i} id={`cards-${i}`}>
+                <QuestionCards cards={cards} onClick={this.onClickCards} newCards />
+              </li>);
+      } else {
+        allCards.push(
+              <li className="card" key={i} id={`cards-${i}`}> 
+                <AnswerCards  cards={cards} onClick={this.onClickCards} />
+              </li>);
+      }   
+    });
+
+    return allCards;
   }
 
   render() {
     return (
-      <div className="App">
-        <Cards cards={this.state.cards} onClick={this.onClick}/>
+      <div>
+        {this.renderBackButton()}
+        <ul className="container">
+          {this.renderAllCards()}
+        </ul>
       </div>
     );
   }
